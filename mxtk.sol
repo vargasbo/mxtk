95ae99a2f605d8216e5d9c5b34d0b544aee0619b
Bo Vargas <vargasbo@users.noreply.github.com>
2024-03-07 13:03:13 -0600

    adjust the users token based on

--- mxtk.sol
+++ mxtk.sol
@@ -594,20 +594,28 @@ OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
 
     function setInitialValues() internal {
         require(!calledOnce,"already called");
-        _mint(0x91852aEC928690F4F55e556c4b000302b04c3e30,4601442839954048884548696); //1
-        _mint(0xb36C15f1ED5cedb9E913218219016d8Cf5Ac864F,4255602869571497331219493); //2
-        _mint(0x121B039CBc60aA1bf563306eB24013D0e1bA0989,2672762772000000000000000); //3
-        _mint(0xB3f46cC55a50225f197AE5a4d1545350f48B2F0b,624477244200000000000000);  //4
 
-        _mint(0xf49a4C1A5250aF8Da4beB67b9C28e82f7D1E8D92,198600000000000000000); //5
-        _mint(0xc2DE3C2143a0c979Ee00019BeDEfF89AE8124262,198600000000000000000); //6
-        _mint(0x4bD9Ea8D612aD197de3d3180db0A60bC7Cbc3189,198600000000000000000); //7
-        _mint(0xB9aD5fd45F3A36Be70E2fD2F661060ddc1D0fc09,198600000000000000000); //8
-        _mint(0xd79497C683BD0eA45DaFc8b732cBf7344F5Df231,3152775000000000000); //9
-        _mint(0xE1a0508BB886C110f2238C0232795c30d99C71D7,93448174827476894);  //10
-        _mint(0xDf0a2F8E8c5a78D1E501382060C803750C5E5821,30783000000000000);  //11
-        _mint(0x5c5Eac6cE39623A023F886eC015C635b04a95f71,13902000000000000);  //12
-        _mint(0xd0684c3311483027bAaFCDd3dB91876BEd5b86c9,4956360431002742);  //13
+        // Calculate the difference in ETH price between original and current
+        uint256 originalEthPrice = 158802000000; // Original ETH price for 9/23/23 when published
+        uint256 currentEthPrice = getETHPrice(); // Current ETH price
+        require(originalEthPrice > 0 && currentEthPrice > 0, "Invalid ETH price");
+
+        uint256 priceDifference = currentEthPrice / originalEthPrice; // Difference as a factor
+
+        _mint(0x91852aEC928690F4F55e556c4b000302b04c3e30,4601442839954048884548696 * priceDifference); //1
+        _mint(0xb36C15f1ED5cedb9E913218219016d8Cf5Ac864F,4255602869571497331219493 * priceDifference); //2
+        _mint(0x121B039CBc60aA1bf563306eB24013D0e1bA0989,2672762772000000000000000 * priceDifference); //3
+        _mint(0xB3f46cC55a50225f197AE5a4d1545350f48B2F0b,624477244200000000000000 * priceDifference);  //4
+
+        _mint(0xf49a4C1A5250aF8Da4beB67b9C28e82f7D1E8D92,198600000000000000000 * priceDifference); //5
+        _mint(0xc2DE3C2143a0c979Ee00019BeDEfF89AE8124262,198600000000000000000 * priceDifference); //6
+        _mint(0x4bD9Ea8D612aD197de3d3180db0A60bC7Cbc3189,198600000000000000000 * priceDifference); //7
+        _mint(0xB9aD5fd45F3A36Be70E2fD2F661060ddc1D0fc09,198600000000000000000 * priceDifference); //8
+        _mint(0xd79497C683BD0eA45DaFc8b732cBf7344F5Df231,3152775000000000000 * priceDifference); //9
+        _mint(0xE1a0508BB886C110f2238C0232795c30d99C71D7,93448174827476894 * priceDifference);  //10
+        _mint(0xDf0a2F8E8c5a78D1E501382060C803750C5E5821,30783000000000000 * priceDifference);  //11
+        _mint(0x5c5Eac6cE39623A023F886eC015C635b04a95f71,13902000000000000 * priceDifference);  //12
+        _mint(0xd0684c3311483027bAaFCDd3dB91876BEd5b86c9,4956360431002742 * priceDifference);  //13
 
         calledOnce = true;
     }
