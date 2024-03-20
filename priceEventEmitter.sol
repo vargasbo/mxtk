9135f03554c3856df7306d788299df47f0f46e93
Bo Vargas <vargasbo@users.noreply.github.com>
2024-03-20 13:55:45 -0500

    fix the price oracle and emmitter

--- priceEventEmitter.sol
+++ priceEventEmitter.sol
@@ -11,7 +11,8 @@ contract PriceEventEmitter is Ownable {
 
     event eventEmitted(string,int256);
 
-    function emitEvent(string memory symbol,int256 price) public onlyOwner {
+    function emitEvent(string memory symbol,int256 price, address ownerAddress) public{
+        require(ownerAddress == owner(), "Only the contract owner can emit the mineral price oracle");
         emit eventEmitted(symbol, price);
     }
 }
